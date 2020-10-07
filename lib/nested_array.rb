8c8005c5bca0bd618dcc7a2a179a84922f484502
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-10-07 16:47:01 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -32,11 +32,14 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
+  content = martrix[row][column]
+  content
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
 end
 
 def matrix_update(matrix, row, column, new_value)
+  martrix[row][column] = new_value
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
